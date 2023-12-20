7acfd00e1018d4cc23525d080a4b2eb29fdbbd05
DongyangLi <62006079+dongyangli-del@users.noreply.github.com>
2023-12-20 17:34:14 +0800

    Add files via upload

---
+++ MAE.py
@@ -0,0 +1,204 @@
+import torch
+import torch.nn as nn
+from timm.models.vision_transformer import PatchEmbed, Block
+from util import get_2d_sincos_pos_embed
+
+
+class MaskedAutoencoderViT(nn.Module):
+    # 初始化方法，参数包括图像尺寸、块大小、通道数、嵌入维度、深度、头数等
+    def __init__(self, img_size=96, patch_size=4, in_chans=3,
+                 embed_dim=256, depth=24, num_heads=16,
+                 decoder_embed_dim=256, decoder_depth=8, decoder_num_heads=16,
+                 mlp_ratio=4., norm_layer=nn.LayerNorm, norm_pix_loss=False):
+        super().__init__()
+
+        self.in_chans = in_chans
+
+        # MAE 编码器
+        self.patch_embed = PatchEmbed(img_size, patch_size, in_chans, embed_dim)
+        num_patches = self.patch_embed.num_patches
+
+        self.cls_token = nn.Parameter(torch.zeros(1, 1, embed_dim))
+        # 初始化一个固定的正弦余弦位置编码
+        self.pos_embed = nn.Parameter(torch.zeros(1, num_patches + 1, embed_dim),
+                                      requires_grad=False)  # fixed sin-cos embedding
+
+        self.blocks = nn.ModuleList([
+            Block(embed_dim, num_heads, mlp_ratio, qkv_bias=True, norm_layer=norm_layer)
+            for i in range(depth)])
+        self.norm = norm_layer(embed_dim)
+
+        # MAE 解码器
+        self.decoder_embed = nn.Linear(embed_dim, decoder_embed_dim, bias=True)
+
+        self.mask_token = nn.Parameter(torch.zeros(1, 1, decoder_embed_dim))
+        # 初始化一个固定的正弦余弦位置编码
+        self.decoder_pos_embed = nn.Parameter(torch.zeros(1, num_patches + 1, decoder_embed_dim),
+                                              requires_grad=False)  # fixed sin-cos embedding
+        # 初始化一系列解码器块
+        self.decoder_blocks = nn.ModuleList([
+            Block(decoder_embed_dim, decoder_num_heads, mlp_ratio, qkv_bias=True, norm_layer=norm_layer)
+            for i in range(decoder_depth)])
+       
+        # 定义解码器的归一化层
+        self.decoder_norm = norm_layer(decoder_embed_dim)
+        # 定义解码器的预测层，将解码器的输出转换回图像块
+        self.decoder_pred = nn.Linear(decoder_embed_dim, patch_size ** 2 * in_chans, bias=True)  # decoder to patch
+
+        # 设置是否对像素损失进行归一化
+        self.norm_pix_loss = norm_pix_loss
+        # 初始化权重
+        self.initialize_weights()
+
+    # 初始化权重的函数
+    def initialize_weights(self):
+        # initialization
+        # initialize (and freeze) pos_embed by sin-cos embedding
+        pos_embed = get_2d_sincos_pos_embed(self.pos_embed.shape[-1], int(self.patch_embed.num_patches**.5), cls_token=True)
+        self.pos_embed.data.copy_(torch.from_numpy(pos_embed).float().unsqueeze(0))
+
+        decoder_pos_embed = get_2d_sincos_pos_embed(self.decoder_pos_embed.shape[-1], int(self.patch_embed.num_patches**.5), cls_token=True)
+        self.decoder_pos_embed.data.copy_(torch.from_numpy(decoder_pos_embed).float().unsqueeze(0))
+
+        # initialize patch_embed like nn.Linear (instead of nn.Conv2d)
+        w = self.patch_embed.proj.weight.data
+        torch.nn.init.xavier_uniform_(w.view([w.shape[0], -1]))
+
+        # timm's trunc_normal_(std=.02) is effectively normal_(std=0.02) as cutoff is too big (2.)
+        torch.nn.init.normal_(self.cls_token, std=.02)
+        torch.nn.init.normal_(self.mask_token, std=.02)
+
+        # initialize nn.Linear and nn.LayerNorm
+        self.apply(self._init_weights)
+
+    # 用于初始化权重的辅助函数
+    def _init_weights(self, m):
+        if isinstance(m, nn.Linear):
+            # we use xavier_uniform following official JAX ViT:
+            torch.nn.init.xavier_uniform_(m.weight)
+            if isinstance(m, nn.Linear) and m.bias is not None:
+                nn.init.constant_(m.bias, 0)
+        elif isinstance(m, nn.LayerNorm):
+            nn.init.constant_(m.bias, 0)
+            nn.init.constant_(m.weight, 1.0)
+
+
+    # patchify函数：将N×3×96×96图像变成图像块，每个图像块大小为4，并将24×24图像拉伸为1×576序列
+    def patchify(self, imgs):
+        p = self.patch_embed.patch_size[0]
+        assert imgs.shape[2] == imgs.shape[3] and imgs.shape[2] % p == 0
+
+        h = w = imgs.shape[2] // p
+        x = imgs.reshape(shape=(imgs.shape[0], self.in_chans, h, p, w, p))
+        x = torch.einsum('nchpwq->nhwpqc', x)
+        x = x.reshape(shape=(imgs.shape[0], h * w, p ** 2 * self.in_chans))
+        return x
+
+    # unpatchify函数：patchify的逆过程，将序列恢复到RGB图像N×3×96×96
+    def unpatchify(self, x):
+        p = self.patch_embed.patch_size[0]
+        h = w = int(x.shape[1] ** .5)
+        assert h * w == x.shape[1]
+
+        x = x.reshape(shape=(x.shape[0], h, w, p, p, self.in_chans))
+        x = torch.einsum('nhwpqc->nchpwq', x)
+        imgs = x.reshape(shape=(x.shape[0], self.in_chans, h * p, h * p))
+        return imgs
+
+    # random_masking函数：执行随机采样。通过均匀分布产生一个随机的数组，然后将之进行高低排序，选择小数值即1/4部分保留，余下的去掉。
+    def random_masking(self, x, mask_ratio):
+        N, L, D = x.shape  # batch, length, dim
+        len_keep = int(L * (1 - mask_ratio))
+
+        noise = torch.rand(N, L, device=x.device)  # noise in [0, 1]
+
+        # sort noise for each sample
+        ids_shuffle = torch.argsort(noise, dim=1)  # ascend: small is keep, large is remove
+        ids_restore = torch.argsort(ids_shuffle, dim=1)
+
+        # keep the first subset
+        ids_keep = ids_shuffle[:, :len_keep]
+        x_masked = torch.gather(x, dim=1, index=ids_keep.unsqueeze(-1).repeat(1, 1, D))
+
+        # generate the binary mask: 0 is keep, 1 is remove
+        mask = torch.ones([N, L], device=x.device)
+        mask[:, :len_keep] = 0
+        # unshuffle to get the binary mask
+        mask = torch.gather(mask, dim=1, index=ids_restore)
+
+        return x_masked, mask, ids_restore
+   
+    # 编码器的前向传播
+    def forward_encoder(self, x, mask_ratio):
+        # 嵌入补丁
+        x = self.patch_embed(x)
+
+        # add pos embed w/o cls token
+        x = x + self.pos_embed[:, 1:, :]
+
+        # masking: length -> length * mask_ratio
+        x, mask, ids_restore = self.random_masking(x, mask_ratio)
+
+        # append cls token
+        cls_token = self.cls_token + self.pos_embed[:, :1, :]
+        cls_tokens = cls_token.expand(x.shape[0], -1, -1)
+        x = torch.cat((cls_tokens, x), dim=1)
+
+        # apply Transformer blocks
+        for blk in self.blocks:
+            x = blk(x)
+        x = self.norm(x)
+
+        return x, mask, ids_restore
+
+    # 解码器的前向传播
+    def forward_decoder(self, x, ids_restore):
+        # embed tokens
+        x = self.decoder_embed(x)
+
+        # append mask tokens to sequence
+        mask_tokens = self.mask_token.repeat(x.shape[0], ids_restore.shape[1] + 1 - x.shape[1], 1)
+        x_ = torch.cat([x[:, 1:, :], mask_tokens], dim=1)  # no cls token
+        x_ = torch.gather(x_, dim=1, index=ids_restore.unsqueeze(-1).repeat(1, 1, x.shape[2]))  # unshuffle
+        x = torch.cat([x[:, :1, :], x_], dim=1)  # append cls token
+
+        # add pos embed
+        x = x + self.decoder_pos_embed
+
+        # apply Transformer blocks
+        for blk in self.decoder_blocks:
+            x = blk(x)
+        x = self.decoder_norm(x)
+
+        # predictor projection
+        x = self.decoder_pred(x)
+
+        # remove cls token
+        x = x[:, 1:, :]
+
+        return x
+
+    def forward_loss(self, imgs, pred, mask):
+        target = self.patchify(imgs)
+        if self.norm_pix_loss:
+            mean = target.mean(dim=-1, keepdim=True)
+            var = target.var(dim=-1, keepdim=True)
+            target = (target - mean) / (var + 1.e-6) ** .5
+
+        loss = (pred - target) ** 2
+        loss = loss.mean(dim=-1)  # [N, L], mean loss per patch
+
+        loss = (loss * mask).sum() / mask.sum()  # mean loss on removed patches
+        return loss
+
+    def forward(self, imgs, mask_ratio=0.75):
+        latent, mask, ids_restore = self.forward_encoder(imgs, mask_ratio)
+        pred = self.forward_decoder(latent, ids_restore)  # [N, L, p*p*3]
+        loss = self.forward_loss(imgs, pred, mask)
+        return loss, pred, mask
+
+if __name__ == "__main__":
+    x = torch.randn(1,3,96,96)
+    net = MaskedAutoencoderViT()
+    _,y,_ = net(x)
+    print(y.shape)
---
+++ sp2.py
@@ -0,0 +1,329 @@
+
+import os
+
+import torch
+import torch.optim as optim
+from torch.nn import CrossEntropyLoss
+from torch.nn import functional as F
+from torch.optim import Adam
+from torch.utils.data import DataLoader
+
+os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+
+from itertools import combinations
+
+import clip
+import matplotlib.pyplot as plt
+import numpy as np
+import torch.nn as nn
+import torchvision.transforms as transforms
+import tqdm
+from cls_eegdatasets import EEGDataset
+from eegencoder import eeg_encoder
+from einops.layers.torch import Rearrange, Reduce
+from lavis.models.clip_models.loss import ClipLoss
+from sklearn.metrics import confusion_matrix
+from torch.utils.data import DataLoader, Dataset
+import random
+from utils import wandb_logger
+
+def make_block(h_c, h_l, dropout_rate=0.4):
+    block = nn.Sequential(
+        nn.LayerNorm(h_l),
+        nn.Linear(h_l, h_l), 
+        nn.GELU(),
+        # nn.Dropout(dropout_rate),
+        Rearrange('B C L->B L C'),
+        nn.LayerNorm(h_c),
+        nn.Linear(h_c, h_c), 
+        nn.GELU(),
+        # nn.Dropout(dropout_rate),
+        Rearrange('B L C->B C L'),
+    )
+    return block
+
+class Projector(nn.Module):
+
+    def __init__(self, in_features, h_dim=(64, 512), n_hidden_layer=4, dropout_rate=0.4):
+        # in_features: (c, l)
+        super().__init__()
+        c, l = in_features
+        h_c, h_l = h_dim
+        c_o, l_o = 1, 512
+
+        self.input_layer = nn.Sequential(
+            nn.LayerNorm(l),
+            nn.Linear(l, h_l), 
+            nn.GELU(),
+            # nn.Dropout(dropout_rate),
+            Rearrange('B C L->B L C'),
+            nn.LayerNorm(c),
+            nn.Linear(c, h_c), 
+            nn.GELU(),
+            # nn.Dropout(dropout_rate),
+            Rearrange('B L C->B C L'),
+        )
+        
+        self.output_layer = nn.Sequential(
+            nn.LayerNorm(h_l),
+            nn.Linear(h_l, l_o), 
+            nn.GELU(),
+            # nn.Dropout(dropout_rate),
+            Rearrange('B C L->B L C'),
+            nn.LayerNorm(h_c),
+            nn.Linear(h_c, c_o), 
+            nn.GELU(),
+            # nn.Dropout(dropout_rate),
+            Rearrange('B L C->B (C L)'),
+        )
+        
+        self.blocks = nn.Sequential(*[
+            make_block(h_c, h_l) for _ in range(n_hidden_layer)
+        ])
+        
+        self.projector = nn.Sequential(*[
+            self.input_layer,
+            self.blocks,
+            self.output_layer,
+        ])
+
+        self.logit_scale = nn.Parameter(torch.ones([]) * np.log(1 / 0.07))
+        self.loss_func = ClipLoss()
+    
+    def forward(self, eeg_embeds):
+        eeg_embeds = self.projector(eeg_embeds)
+        eeg_features = F.normalize(eeg_embeds, dim=-1)
+        return eeg_features
+
+class eegEncoder(nn.Module):
+
+    def __init__(self, in_features):
+        super().__init__()
+        self.mae_encoder = eeg_encoder() 
+        self.projector = Projector(in_features)
+        self.freeze_mae()
+        self.logit_scale = self.projector.logit_scale
+        self.loss_func = self.projector.loss_func
+
+    def freeze_mae(self):
+        for param in self.mae_encoder.parameters():
+            param.requires_grad = False
+
+    def forward(self, eeg):
+        eeg_embeds = self.mae_encoder(eeg)
+        eeg_features = self.projector(eeg_embeds)
+        return eeg_features
+
+
+def train_model(model, dataloader, optimizer, device, text_features_all, img_features_all):
+    model.train()
+    text_features_all = text_features_all.to(device).float() # (n_cls, d)
+    img_features_all = img_features_all.to(device).float()
+    total_loss = 0
+    correct = 0
+    total = 0
+    for batch_idx, (eeg_data, labels, text, text_features, img, img_features) in enumerate(dataloader):
+        eeg_data = eeg_data.to(device)
+        text_features = text_features.to(device).float()
+        img_features = img_features.to(device).float()
+        labels = labels.to(device)
+        
+        optimizer.zero_grad()
+        eeg_features = model(eeg_data).float()
+        logit_scale = model.logit_scale
+        
+        loss = model.loss_func(eeg_features, img_features, logit_scale)
+        loss.backward()
+
+        optimizer.step()
+        total_loss += loss.item()
+        
+        logits = logit_scale * eeg_features @ img_features_all.T # (n_batch, n_cls)
+        predicted = torch.argmax(logits, dim=1) # (n_batch, ) \in {0, 1, ..., n_cls-1}
+
+        batch_size = predicted.shape[0]
+        total += batch_size
+        correct += (predicted == labels).sum().item()
+
+    average_loss = total_loss / (batch_idx+1)
+    accuracy = correct / total
+    return average_loss, accuracy
+
+def evaluate_model(model, dataloader, device, text_features_all, img_features_all):
+    model.eval()
+    text_features_all = text_features_all.to(device).float()
+    img_features_all = img_features_all.to(device).float()
+    total_loss = 0
+    correct = 0
+    total = 0
+   
+    with torch.no_grad():
+        for batch_idx, (eeg_data, labels, text, text_features, img, img_features) in enumerate(dataloader):
+            eeg_data = eeg_data.to(device)
+            text_features = text_features.to(device).float()
+            img_features = img_features.to(device).float()
+            labels = labels.to(device)
+        
+            eeg_features = model(eeg_data).float()
+            logit_scale = model.logit_scale 
+
+            loss = model.loss_func(eeg_features, img_features, logit_scale)
+         
+            total_loss += loss.item()
+            
+            logits = logit_scale * eeg_features @ img_features_all.T # (n_batch, n_cls)
+            predicted = torch.argmax(logits, dim=1) # (n_batch, ) \in {0, 1, ..., n_cls-1}
+            predicted = (predicted + 1) % 2
+
+            batch_size = predicted.shape[0]
+            total += batch_size
+            correct += (predicted == labels).sum().item()
+            print(predicted)
+            print(labels)
+
+    average_loss = total_loss / (batch_idx+1)
+    accuracy = correct / total
+    return average_loss, accuracy
+
+def main_train_loop(model, train_dataloader, test_dataloader, optimizer, device, 
+                    text_features_train_all, text_features_test_all, img_features_train_all, img_features_test_all, config, logger=None):
+    logger = wandb_logger(config) if logger else None
+    logger.watch(model,logger) 
+    
+    train_losses, train_accuracies = [], []
+    test_losses, test_accuracies = [], []
+    
+
+    best_accuracy = 0.0
+    best_model_weights = None
+    best_epoch_info = {}
+
+    for epoch in range(config['epochs']):
+        # 训练模型
+        train_loss, train_accuracy = train_model(model, train_dataloader, optimizer, device, text_features_train_all, img_features_train_all)
+        train_losses.append(train_loss)
+        train_accuracies.append(train_accuracy)
+
+        # 评估模型
+        test_loss, test_accuracy = evaluate_model(model, test_dataloader, device, text_features_test_all, img_features_test_all)
+ 
+        test_losses.append(test_loss)
+        test_accuracies.append(test_accuracy)
+      
+
+        # 如果当前epoch的测试正确率是最好的，保存模型和相关信息
+        if test_accuracy > best_accuracy:
+            best_accuracy = test_accuracy
+            best_model_weights = model.state_dict().copy()
+            best_epoch_info = {
+                "epoch": epoch + 1,
+                "train_loss": train_loss,
+                "train_accuracy": train_accuracy,
+                "test_loss": test_loss,
+                "test_accuracy": test_accuracy,
+                
+            }
+        logger.log({
+            "Train Loss": train_loss,
+            "Train Accuracy": train_accuracy,
+            "Test Loss": test_loss,
+            "Test Accuracy": test_accuracy,
+            
+            "Epoch": epoch
+        })
+
+        print(f"Epoch {epoch + 1}/{config['epochs']} - Train Loss: {train_loss:.4f}, Train Accuracy: {train_accuracy:.4f}, Test Loss: {test_loss:.4f}, Test Accuracy: {test_accuracy:.4f}")
+        
+    
+    # 加载最佳模型权重
+    model.load_state_dict(best_model_weights)
+
+    # 保存最佳模型
+    # torch.save(model.state_dict(), 'best_model_lr=3e-4_img_maskeeg.pth')
+
+# 创建5个子图
+    fig, axs = plt.subplots(3, 2, figsize=(10, 15))
+
+    # 损失图
+    axs[0, 0].plot(train_losses, label='Train Loss')
+    axs[0, 0].plot(test_losses, label='Test Loss')
+    axs[0, 0].legend()
+    axs[0, 0].set_title("Loss Curve")
+
+    # 总体正确率图
+    axs[0, 1].plot(train_accuracies, label='Train Accuracy')
+    axs[0, 1].plot(test_accuracies, label='Test Accuracy')
+    axs[0, 1].legend()
+    axs[0, 1].set_title("Accuracy Curve")
+
+
+    # 构造你要注释的字符串信息
+    info_text = (f"Best Model Info (from Epoch {best_epoch_info['epoch']}):\n"
+                f"Train Loss: {best_epoch_info['train_loss']:.4f}\n"
+                f"Train Accuracy: {best_epoch_info['train_accuracy']:.4f}\n"
+                f"Test Loss: {best_epoch_info['test_loss']:.4f}\n"
+                f"Test Accuracy: {best_epoch_info['test_accuracy']:.4f}\n"
+               )
+
+    axs[2, 1].axis('off')  
+    axs[2, 1].text(0.5, 0.5, info_text, fontsize=10, ha='center', va='center', transform=axs[2, 1].transAxes)
+
+
+    # 添加大标题
+    plt.suptitle('best_model_lr=3e-4_img_maskeeg', fontsize=16, y=1.05)
+    plt.savefig('best_model_lr=3e-4_img_maskeeg.png')
+    logger.log({"Plots": logger.Image(plt)})
+    logger.finish()
+
+def main():
+    # config = {
+    #     'project': 'eeg_clf',
+    #     'entity': 'vis-obj',
+    #     'name': '特殊图片二分类', 
+    #     'lr': 3e-3, 
+    #     'epochs': 50, 
+    #     'batch_size': 512, 
+    #     'logger': True, 
+    # }
+    config = {
+        # 'project': 'eeg_clf',
+        # 'entity': 'vis-obj',
+        # 'name': 'lr=3e-4_img_maskeeg_注意机制2', 
+        'project': 'BCI',
+        'entity': 'sustech_rethinkingbci',
+        'name': '', 
+        
+        'lr': 3e-4, 
+        'epochs': 50, 
+        'batch_size': 512, 
+        'logger': True, 
+    }
+   # Instantiate the dataset and dataloader
+    data_path = '/home/geek/Workspace/BCI/Data/THINGS/EEG/osfstorage-archive'  # Replace with the path to your data
+    train_dataset = EEGDataset(data_path, train=True)
+    test_dataset = EEGDataset(data_path, train=True, classes = [920, 1292],pictures=[4, 9])
+    train_loader = DataLoader(train_dataset, batch_size=512, shuffle=True, num_workers=0, drop_last=True)
+    test_loader = DataLoader(test_dataset, batch_size=512, shuffle=True, num_workers=0, drop_last=False)
+    
+    text_features_train_all = train_dataset.text_features
+    text_features_test_all = test_dataset.text_features
+    img_features_train_all = train_dataset.img_features
+    img_features_test_all = test_dataset.img_features
+    
+    
+    # 模型和优化器定义
+    in_features = (17, 512)
+    model = eegEncoder(in_features)
+    optimizer = torch.optim.AdamW(model.parameters(), lr=3e-3)
+    
+    print('number of parameters:', sum([p.numel() for p in model.parameters()]))
+
+    # 确定使用的设备
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model.to(device)
+    
+    main_train_loop(model, train_loader, test_loader, optimizer, device, 
+                    text_features_train_all, text_features_test_all, img_features_train_all, img_features_test_all, config, logger=config['logger'])
+    
+if __name__ == '__main__':
+    main()
