ecd5fd5cd799d9ed1e3119cab8cb1c06dd959f45
dongyangli <2250431442@qq.com>
2024-05-19 15:39:53 +0800

    update codes

--- Generation/fMRI-reconstruction-NSD/src/download.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -e
-
-mkdir -p checkpoints
-cd checkpoints
-wget https://huggingface.co/nousr/conditioned-prior/resolve/main/vit-l-14/aesthetic/best.pth
-wget https://huggingface.co/nousr/conditioned-prior/raw/main/vit-l-14/aesthetic/prior_config.json
