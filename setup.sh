86795996913b3d190f18cf2c3611276cabdf05be
dongyangli <2250431442@qq.com>
2024-09-28 09:34:39 +0800

    update readme

--- setup.sh
+++ setup.sh
@@ -21,10 +21,10 @@ pip install diffusers==0.24.0
 pip install braindecode==0.8.1
 
 pip install torchvision==0.15.2 torch==2.0.1
-pip install diffusers==0.13.0
 
 pip install info-nce-pytorch==0.1.0
 pip install pytorch-msssim
 
+pip install mne
 pip install wandb
 pip install einops
